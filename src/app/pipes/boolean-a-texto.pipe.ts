6e069d9773ff8bd035c4133414359e384c95feb1
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-10-14 00:05:04 -0300

    pipe

--- src/app/pipes/boolean-a-texto.pipe.ts
+++ src/app/pipes/boolean-a-texto.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BooleanATextoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: boolean, ...args: string[]): string {
+    if(value){
+      return args[0];
+    }else{
+    return args[1];}
+
+
   }
 
 }
