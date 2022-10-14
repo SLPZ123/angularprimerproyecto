d8ba7a9d83f272a8afbdab6c85675955e3ed6a41
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-10-13 23:15:12 -0300

    entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { DemoTableComponent } from './component/demo-table/demo-table.component';
+import { DemoCardComponent } from './component/demo-card/demo-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DemoTableComponent,
+    DemoCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/curso.ts
@@ -0,0 +1,11 @@
+export interface Curso {
+    nombre:string;
+    comision:string;
+    profesor:string,
+    fechaInicio:Date;
+    fechaFin:Date;
+    inscripcionAbierta:boolean;
+    imagen:string;
+
+
+}
